0f109cd6c8c32755f83664c33e9fb089f9210235
Jan Johannes <jan@ntr.io>
2024-06-04 00:07:53 +0300

    minimal structure for app

---
+++ src/routes/repos/+page.svelte
@@ -0,0 +1,3 @@
+list of opened/ available repos
+
+<p><a href="/repos/github.com/opral/monorepo.git">/github.com/opral/monorepo.git</a></p>
---
+++ src/service-worker.js
@@ -0,0 +1,8 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+
+const sw = /** @type {ServiceWorkerGlobalScope} */ (/** @type {unknown} */ (self));
+
+console.log('Hello from service worker!', sw);
